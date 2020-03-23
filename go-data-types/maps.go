da041688d93aed2ea17f0cfaf3b84a4a1d2c5cdf
Nathan Melehan <nmelehan@gmail.com>
2020-03-24 00:30:38 +0300

    Go Data Types test files

--- go-data-types/maps.go
+++ go-data-types/maps.go
@@ -8,31 +8,31 @@ func main() {
     mapA := make(map[string]int)
     mapA["k1"] = 12
     mapA["k2"] = 13
-	fmt.Println("mapA:", mapA)
-	fmt.Println()
+    fmt.Println("mapA:", mapA)
+    fmt.Println()
 
     mapB := map[string]int{
         "k1": 12,
         "k2": 13,
-	}
+    }
 
-	fmt.Println("mapB:", mapB)
-	fmt.Println("Deleting key `k1` from mapB...")
+    fmt.Println("mapB:", mapB)
+    fmt.Println("Deleting key `k1` from mapB...")
     delete(mapB, "k1")
     delete(mapB, "k1")
-	fmt.Println("mapB:", mapB)
-	fmt.Println()
+    fmt.Println("mapB:", mapB)
+    fmt.Println()
 
-	fmt.Println("Checking if the `doesItExist` key exists in mapA...")
+    fmt.Println("Checking if the `doesItExist` key exists in mapA...")
     _, ok := mapA["doesItExist"]
     if ok {
         fmt.Println("Exists!")
     } else {
         fmt.Println("Does NOT exist")
-	}
-	fmt.Println()
+    }
+    fmt.Println()
 
-	fmt.Println("Iterating through keys and values of mapA...")
+    fmt.Println("Iterating through keys and values of mapA...")
     for key, value := range mapA {
         fmt.Println(key, ":", value)
     }
